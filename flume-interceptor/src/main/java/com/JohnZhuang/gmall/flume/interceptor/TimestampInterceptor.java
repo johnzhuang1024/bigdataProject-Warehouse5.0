3b1bebf70d3c7c59fc3a9bec206ffe8c1674a4cf
JohnZhuang <johnzhuang1024@gmail.com>
2023-02-06 21:43:47 +0800

    TimestampInterceptor拦截器

---
+++ flume-interceptor/src/main/java/com/JohnZhuang/gmall/flume/interceptor/TimestampInterceptor.java
@@ -0,0 +1,66 @@
+package com.JohnZhuang.gmall.flume.interceptor;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.interceptor.Interceptor;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Description:
+ * @Author: JohnZhuang1024
+ * @Date: 2023/2/6 21:41
+ * @Version：1.0
+ */
+public class TimestampInterceptor implements Interceptor {
+
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public Event intercept(Event event) {
+
+        //1、获取header和body的数据
+        Map<String, String> headers = event.getHeaders();
+        String log = new String(event.getBody(), StandardCharsets.UTF_8);
+
+        //2、将body的数据类型转成jsonObject类型（方便获取数据）
+        JSONObject jsonObject = JSONObject.parseObject(log);
+
+        //3、header中timestamp时间字段替换成日志生成的时间戳（解决数据漂移问题）
+        String ts = jsonObject.getString("ts");
+        headers.put("timestamp", ts);
+
+        return event;
+    }
+
+    @Override
+    public List<Event> intercept(List<Event> list) {
+        for (Event event : list) {
+            intercept(event);
+        }
+        return list;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    public static class Builder implements Interceptor.Builder {
+        @Override
+        public Interceptor build() {
+            return new TimestampInterceptor();
+        }
+
+        @Override
+        public void configure(Context context) {
+        }
+    }
+}
+
